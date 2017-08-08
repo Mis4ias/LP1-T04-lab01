ed81af39341a074d49dc7d3b4dfca0e18cbafbed
João <joaopedroorc@gmail.com>
2017-08-08 00:22:45 -0300

    conversor: projeto modelo do lab01.

---
+++ palidromo/application/lab01.cpp
@@ -0,0 +1,11 @@
+#include "palindromo.h"
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char * argv[])
+{
+    
+    return 0;
+}
+
---
+++ palidromo/include/palindromo.h
@@ -0,0 +1,11 @@
+#ifndef __PALINDROMO_H__
+#define __PALINDROMO_H__
+#include <string>
+
+using namespace std;
+
+bool palindromo (std::string palavra);
+void swap (string &a, string &b);
+bool frase_palidromo(string frase);
+bool anagrama(string str2, string str3);
+#endif /* __PALINDROMO_H__ */
